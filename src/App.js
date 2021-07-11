c1758a1c8ca30ddd813cf7d14822f47a3fd8a640
Philipp Wille <philipp.y.wille@gmail.com>
2021-07-11 18:06:48 +0200

    Remove boilerplate

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { connect } from "react-redux";
 
-function AppPresentation({ hello }) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button id="hello">Hello {hello}!</Button>
-      </header>
-    </div>
-  );
+function AppPresentation() {
+  return <div className="App"></div>;
 }
 
-const ButtonPresentation = ({ children, onClick }) => (
-  <button onClick={onClick}>{children}</button>
-);
-
-const Button = connect(
-  (state) => state,
-  (dispatch, { id }) => ({
-    onClick() {
-      dispatch({ type: "BUTTON_CLICKED", context: { id } });
-    },
-  })
-)(ButtonPresentation);
-
-const App = connect((state) => ({ hello: state.hello }))(AppPresentation);
+const App = connect((state) => state)(AppPresentation);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
-import { take, put } from "redux-saga/effects";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -19,8 +17,6 @@ const persistedState = localStorage.getItem("reduxState")
 
 const rootReducer = (state, action) => {
   switch (action.type) {
-    case "HELLO":
-      return { ...state, hello: state.hello === "Redux" ? "Saga" : "Redux" };
     default:
       return state;
   }
@@ -42,16 +38,6 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-const helloSaga = function* () {
-  while (true) {
-    const { context } = yield take("BUTTON_CLICKED");
-    yield put({ type: "HELLO", params: context });
-  }
-};
+const helloSaga = function* () {};
 
 sagaMiddleware.run(helloSaga);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
