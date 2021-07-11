028266af6fa409d0370b55c22a7b1e8f24550cb9
Philipp Wille <philipp.y.wille@gmail.com>
2021-07-11 20:56:07 +0200

    Use immer in reducers

--- src/store/state.js
+++ src/store/state.js
@@ -1,26 +1,24 @@
 import { unixMillisNow } from "../utils";
+import produce from "immer";
 
-const root = (state, action) => {
+const identity = (a) => a;
+
+const immer = (f) => (state, action) => produce(f(action) || identity)(state);
+
+const root = immer((action) => {
   switch (action.type) {
     case "WIKI_PAGE/SET":
-      return {
-        ...state,
-        pages: {
-          ...state.pages,
-          [action.payload.page]: {
-            ...action.payload.data,
-            lastVisit: unixMillisNow(),
-          },
-        },
+      return (state) => {
+        state.pages[action.payload.page] = {
+          ...action.payload.data,
+          lastVisit: unixMillisNow(),
+        };
       };
     case "FACTIONS/SET":
-      return {
-        ...state,
-        factions: action.payload.factions,
+      return (state) => {
+        state.factions = action.payload.factions;
       };
-    default:
-      return state;
   }
-};
+});
 
 export { root };
