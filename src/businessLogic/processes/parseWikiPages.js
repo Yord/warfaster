aa94c167941eb11610d96b3bd14c5c65cb0ed0bb
Philipp Wille <philipp.y.wille@gmail.com>
2022-07-12 15:45:56 +0200

    Streamline parse wiki pages

--- src/businessLogic/processes/parseWikiPages.js
+++ src/businessLogic/processes/parseWikiPages.js
@@ -15,91 +15,42 @@ import { Models } from "../../state/Models";
 import { WildCardModels } from "../../state/WildCardModels";
 
 function* parseWikiPages() {
-  yield all([
-    parseCypher(),
-    parseCypherCodecs(),
-    parseFactions(),
-    parseFactionModels(),
-    parseModel(),
-    parseWildCard(),
-  ]);
-}
-
-export { parseWikiPages };
-
-function* parseCypher() {
-  while (true) {
-    const { payload } = yield take("WIKI_PAGE/FETCHED");
-    const { data, page } = payload;
-    const pages = yield select(CypherCodecs.selectPages());
-
-    if (pages.includes(page)) {
-      const cypher = parseCypherText(data.text);
-      cypher.name = { text: data.title, page };
-      yield put(Cyphers.set({ page, cypher }));
-    }
-  }
-}
-
-function* parseCypherCodecs() {
   while (true) {
     const { payload } = yield take("WIKI_PAGE/FETCHED");
     const { data, page } = payload;
 
-    if (page === "Cypher_Codecs") {
-      const cypherCodecs = parseCypherCodecsText(data.text);
-      yield put(CypherCodecs.set({ cypherCodecs }));
-    }
-  }
-}
+    const cypherCodecs = yield select(CypherCodecs.selectPages());
+    const factions = yield select(Factions.selectPages());
 
-function* parseFactionModels() {
-  while (true) {
-    const { payload } = yield take("WIKI_PAGE/FETCHED");
-    const { data, page } = payload;
-    const pages = yield select(Factions.selectPages());
-
-    if (pages.includes(page)) {
-      const factionModels = parseFactionModelsText(data.text);
-      yield put(FactionModels.set({ page, factionModels }));
-    }
-  }
-}
-
-function* parseFactions() {
-  while (true) {
-    const { payload } = yield take("WIKI_PAGE/FETCHED");
-    const { data, page } = payload;
+    // TODO: Why Object.values?
+    const factionModels = Object.values(
+      yield select(FactionModels.selectModelPages())
+    );
 
     if (page === "Warcaster") {
       const factions = parseFactionsText(data.text);
       yield put(Factions.set({ factions }));
-    }
-  }
-}
-
-function* parseModel() {
-  while (true) {
-    const { payload } = yield take("WIKI_PAGE/FETCHED");
-    const { data, page } = payload;
-    const pages = Object.values(yield select(FactionModels.selectModelPages()));
-
-    if (pages.includes(page)) {
+    } else if (page === "Wild_Card") {
+      const wildCards = parseWildCardText(data.text);
+      yield put(WildCardModels.set({ wildCards }));
+    } else if (factionModels.includes(page)) {
       const model = parseModelText(data.text);
       model.name = { text: data.title, page };
       yield put(Models.set({ page, model }));
+    } else if (factions.includes(page)) {
+      const factionModels = parseFactionModelsText(data.text);
+      yield put(FactionModels.set({ page, factionModels }));
+    } else if (page === "Cypher_Codecs") {
+      const cypherCodecs = parseCypherCodecsText(data.text);
+      yield put(CypherCodecs.set({ cypherCodecs }));
+    } else if (cypherCodecs.includes(page)) {
+      const cypher = parseCypherText(data.text);
+      cypher.name = { text: data.title, page };
+      yield put(Cyphers.set({ page, cypher }));
+    } else {
+      // TODO: Cannot parse WikiPage
     }
   }
 }
 
-function* parseWildCard() {
-  while (true) {
-    const { payload } = yield take("WIKI_PAGE/FETCHED");
-    const { data, page } = payload;
-
-    if (page === "Wild_Card") {
-      const wildCards = parseWildCardText(data.text);
-      yield put(WildCardModels.set({ wildCards }));
-    }
-  }
-}
+export { parseWikiPages };
