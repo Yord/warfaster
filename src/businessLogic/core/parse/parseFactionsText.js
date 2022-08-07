d51258a23ef9bf8e3690267491c9c33ea50d1cf4
Philipp Wille <philipp.y.wille@gmail.com>
2022-08-07 11:21:55 +0200

    Improve factions parsing

--- src/businessLogic/core/parse/parseFactionsText.js
+++ src/businessLogic/core/parse/parseFactionsText.js
@@ -1,3 +1,4 @@
+import { parseAnchor } from "./parsers";
 import { prepareDOM } from "./utils";
 
 const parseFactionsText = (text) => {
@@ -5,7 +6,7 @@ const parseFactionsText = (text) => {
 
   const p = doc.querySelector("h2#Factions ~ p");
   const factions = Object.fromEntries(collectFactions(p, []));
-  delete factions.Wild_Card;
+  delete factions.Wild_Card; // Wild Cards are crawled independently.
   return factions;
 };
 
@@ -15,10 +16,12 @@ function collectFactions(node, factions) {
   switch (node.tagName) {
     case "P":
       const a = node.querySelector("a");
-      const hrefTitle = a.href.split("title=");
-      const page = hrefTitle[hrefTitle.length - 1];
-      const text = a.innerText;
-      const faction = [page, { text, page }];
+      if (!a) {
+        console.error("collectFactions: a not found!");
+        return collectFactions(node.nextSibling, factions);
+      }
+      const anchor = parseAnchor(a);
+      const faction = [anchor.page, anchor];
       return collectFactions(node.nextSibling, [...factions, faction]);
     case "H2":
       return factions;
