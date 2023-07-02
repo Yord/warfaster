928de67722b5f4b967810828c3a1ec22f21086ed
Philipp Wille <philipp.y.wille@gmail.com>
2023-07-02 15:16:50 +0200

    Increase version due to different state structure in models

--- src/state/Version.js
+++ src/state/Version.js
@@ -5,5 +5,5 @@ const Version = StateShard("Version", init, {}, {}, {});
 export { Version };
 
 function init(state) {
-  state.version = "2023-07-01T20:37:00.000Z";
+  state.version = "2023-07-02T15:16:00.000Z";
 }
